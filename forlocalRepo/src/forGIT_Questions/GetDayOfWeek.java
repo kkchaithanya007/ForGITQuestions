7079e17d8aeec7628ddc9779280d2b498c538c50
kkchaithanya001 <kkchaithanya200@gmail.com>
2023-07-15 18:58:09 +0530

    4. Take a number from user If the number is between 1 and 7 then print the days of the week accordingly For example - if user gives 1 then print Monday if 2 print Tuesday and so on And if the user gives any number less than 1 or greater than 7 then print "Invalid Number".

--- forlocalRepo/src/forGIT_Questions/GetDayOfWeek.java
+++ forlocalRepo/src/forGIT_Questions/GetDayOfWeek.java
@@ -2,31 +2,39 @@ package forGIT_Questions;
 
 import java.util.Scanner;
 
+
 public class GetDayOfWeek 
 {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number from 1 to 7");
+		System.out.println("Enter a number from 1 to 7 ");
 		int a=sc.nextInt();
 		
 			switch(a)
 			{
 			case 1:
 				System.out.println("Mon");
+				break;
 			case 2:
 				System.out.println("Tue");
-			case 3:
+				break;
+			case 3: 
 				System.out.println("Wen");
+				break;
 			case 4:
 				System.out.println("Thu");
+				break;
 			case 5:
 				System.out.println("Fri");
+				break;
 			case 6:
 				System.out.println("Sat");
+				break;
 			case 7:
 				System.out.println("Sun");
-				default:
+				break;
+			default:
 					System.out.println("Invalid");
 		}
 	}
