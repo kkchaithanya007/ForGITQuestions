f8cda3fc95a65552fdb60287fb841983e716599e
kkchaithanya001 <kkchaithanya200@gmail.com>
2023-07-15 13:53:47 +0530

    6. WAP to print square of a number (user input).

---
+++ forlocalRepo/src/forGIT_Questions/SquareQ6.java
@@ -0,0 +1,17 @@
+package forGIT_Questions;
+
+import java.util.Scanner;
+
+public class SquareQ6 
+{
+	/*
+	 * 6. WAP to print square of a number (user input).
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Give an I/P to get square of that a number ");
+		int s=sc.nextInt();
+		System.out.println("For s="+s+" Square is ="+s*s);
+	}
+}
