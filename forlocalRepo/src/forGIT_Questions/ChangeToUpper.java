e7af7c5b05688f53e301e24fdb24348a1211506b
kkchaithanya001 <kkchaithanya200@gmail.com>
2023-07-17 19:42:36 +0530

    35. Write a Java program to convert all the characters in a string to uppercase.

---
+++ forlocalRepo/src/forGIT_Questions/ChangeToUpper.java
@@ -0,0 +1,13 @@
+package forGIT_Questions;
+
+public class ChangeToUpper {
+/*
+ * 35. Write a Java program to convert all the characters in a string to uppercase.
+ */
+	public static void main(String[] args) {
+		String s="hola kkc wht'sApp";
+		System.out.println("Change to UpperCase\n"+s);
+		System.out.println("Changed->"+s.toUpperCase());
+		
+	}
+}
