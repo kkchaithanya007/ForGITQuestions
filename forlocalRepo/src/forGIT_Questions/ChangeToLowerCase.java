37b8618ed1a5dcefea42d6bf3fd3884c34d458b1
kkchaithanya001 <kkchaithanya200@gmail.com>
2023-07-17 19:42:08 +0530

    34. Write a Java program to convert all the characters in a string to lowercase.

---
+++ forlocalRepo/src/forGIT_Questions/ChangeToLowerCase.java
@@ -0,0 +1,13 @@
+package forGIT_Questions;
+
+public class ChangeToLowerCase 
+{
+/*
+ * 34. Write a Java program to convert all the characters in a string to lowercase.
+ */
+	public static void main(String[] args) {
+		String s="HOLA amigo";
+		System.out.println("changing the String="+s+" to a lowercase");
+		System.out.println("Changed String  ===>"+s.toLowerCase());
+	}
+}
