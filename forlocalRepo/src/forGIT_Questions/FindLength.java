2e3f632f688b810c1ca4ece369a7fe9cd8ee8337
kkchaithanya001 <kkchaithanya200@gmail.com>
2023-07-17 12:46:23 +0530

    28. Write a program to find the length of the string "refrigerator".

---
+++ forlocalRepo/src/forGIT_Questions/FindLength.java
@@ -0,0 +1,12 @@
+package forGIT_Questions;
+
+public class FindLength 
+{
+/*
+ * 28. Write a program to find the length of the string "refrigerator".
+ */
+	public static void main(String[] args) {
+		String s="refrigerator";
+		System.out.println(s.length());
+	}
+}
